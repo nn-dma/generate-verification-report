e4998064eacf08f9c9bd91b8e011f39ad107e009
Lasse Lund Sten Jensen <32486318+lasselundstenjensen@users.noreply.github.com>
2024-03-21 00:39:31 +0100

    fix: Always use validation environment for hardcoded tests.

--- test/integration/report_file_in_output_dir_is_not_empty_after_run_test.go
+++ test/integration/report_file_in_output_dir_is_not_empty_after_run_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestReportFileIsNotEmpty(t *testing.T) {
 	// Set up the test case
-	reportFile := "VerificationReport_production_Dummy1234567890_dummy_environment_name.html"
+	reportFile := "VerificationReport_validation_Dummy1234567890_dummy_environment_name.html"
 	outputDir := "output"
 	filePath := "../../" + outputDir + "/" + reportFile
 
