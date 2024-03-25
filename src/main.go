9aa7d6cc536297f6848b69fef0f5857e29c9c27b
Lasse Lund Sten Jensen <32486318+lasselundstenjensen@users.noreply.github.com>
2024-03-25 23:48:25 +0100

    feat: Add execution of ported script and render injection into report file.

--- src/main.go
+++ src/main.go
@@ -184,7 +184,14 @@ func GenerateVerificationReport(ctx context.Context) error {
 
 	log.Info().Msg("Extracting and rendering requirements")
 	generator = generator.
-		WithExec([]string{"sh", "-c", "echo '================> " + color.Purple("Extracting and rendering requirements'")})
+		WithExec([]string{"sh", "-c", "echo '================> " + color.Purple("Extracting and rendering requirements'")}).
+		WithExec([]string{"sh", "-c", "cd", RepositoryDir}).
+		WithExec([]string{"python", path.Join("../", parameters.RenderRequirementsPyLocation), "-folder", RequirementsDir, "-branch", "${GITHUB_REF_NAME}", "-organization", "innersource-nn", "-repository", "${GITHUB_REPOSITORY}"}, dagger.ContainerWithExecOpts{RedirectStdout: path.Join(ArtifactDir, "listOfRequirementsHtml.html")}).
+		WithExec([]string{"sh", "-c", "cd", ".."}).
+		WithExec([]string{"ls", "-la", ArtifactDir}).
+		WithExec([]string{"cat", path.Join(ArtifactDir, "listOfRequirementsHtml.html")}).
+		WithExec([]string{"python", parameters.RenderReplacePyLocation, "-render", path.Join(ArtifactDir, "listOfRequirementsHtml.html"), "-template", "output/report.html", "-placeholder", "<var>LIST_OF_REQUIREMENTS</var>"})
+		// python3 ../../src/script/render_requirements_for_github.py -folder requirements -branch main -organization innersource-nn -repository qms-reference
 	// TODO: Port to GitHub format + write tests
 	/*
 		python3 ../${{ parameters.render_requirements_py_location }} -folder ${{ parameters.feature_files_path }} -branch origin/release/$(Build.SourceBranchName) -organization novonordiskit -project '$(System.TeamProject)' -repository $(Build.Repository.Name) > listOfRequirementsHtml.html
