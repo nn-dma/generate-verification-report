8b5d6c6bd4279d9f4fd155adc3ffcf8107d76d3c
Lasse Lund Sten Jensen <32486318+lasselundstenjensen@users.noreply.github.com>
2024-05-01 14:32:17 +0200

    fix: Add additional guard clause in case the pull request details are malformed or not JSON.

--- src/script/get_pull_request_it_change_issue_github.sh
+++ src/script/get_pull_request_it_change_issue_github.sh
@@ -3,9 +3,16 @@
 if [[ -n $pr ]]; then
     # Code to execute if $pr is set and has a value
     prItChangeIssueNumber=$(jq -r '. | select(.title != null) | .title | match("(IT Change) #\\d+"; "g") | .string | sub(".+ #";"")' <<< $pr)
-    echo $prItChangeIssueNumber
+    # Check if the command was successful
+    if [ $? -ne 0 ]; then
+        echo "error: could not extract 'IT Change' issue number from pull request details"
+        echo "given: '$pr'"
+        exit 1
+    else
+        echo $prItChangeIssueNumber
+    fi
 else
     # Code to execute if $pr is not set or has no value
-    echo "'pr' variable is not set or has no value"
-    #exit 1
+    echo "error: 'pr' variable is not set or has no value"
+    exit 1
 fi
